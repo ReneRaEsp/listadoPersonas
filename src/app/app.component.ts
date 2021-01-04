70bb6fc84e93d793f7b4ea1ad931fa931b96d221
Rene <rdejramirez@gmail.com>
2021-01-04 14:14:50 -0500

    Listado de personas en construccion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Persona } from './persona.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'listadoPersonas';
+  titulo = 'Listado de Personas';
+
+  personas: Persona[] = [new Persona("Juan","Perez"), new Persona("Laura","Gomez")];
+  
+
+ onPersonaAgregada(persona: Persona){
+   this.personas.push(persona);
+
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { PersonaComponent } from './persona/persona.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonaComponent,
+    FormularioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
