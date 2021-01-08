2c18ea4b06a038ca03c4a18fd8ca83dcdfb97f0b
Rene <rdejramirez@gmail.com>
2021-01-07 23:03:41 -0500

    Cambiado a ViewChild

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 import { Persona } from '../persona.model';
 
 @Component({
@@ -10,13 +10,15 @@ export class FormularioComponent implements OnInit {
   @Output() personaCreada = new EventEmitter<Persona>();
   //nombreInput:string;
   //apellidoInput:string;
+  @ViewChild('nombreInput') nombreInput: ElementRef;
+  @ViewChild('apellidoInput') apellidoInput: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-   onAgregarPersona(nombreInput:HTMLInputElement, apellidoInput:HTMLInputElement){
-  	let persona1 = new Persona(nombreInput.value, apellidoInput.value);
+   onAgregarPersona(){
+  	let persona1 = new Persona(this.nombreInput.nativeElement.value, this.apellidoInput.nativeElement.value);
   	this.personaCreada.emit(persona1);
   }
 
